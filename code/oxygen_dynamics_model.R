33a91d26fb49910cff6af0331efaccb1eef7f5f1
Alice Carter <alicecarter05@gmail.com>
2021-06-23 18:13:43 -0600

    initializing project for creating process models of dissolved oxygen

---
+++ code/oxygen_dynamics_model.R
@@ -0,0 +1,17 @@
+# modeled oxygen dynamics in streams:
+
+# mass balance equation:
+# dVC/dt = -(dq_x/dx + dq_y/dy + dq_z/dz) + S
+#   V = volume
+#   C = concentration
+#   q_xyz = mass fluxes due to advective and dispersive transport
+#   S = sinks and sources
+
+
+# Assumptions:
+# 1. uniform flow/well mixed:
+#   dq_x/dx + dq_y/dy = 0
+# Physical processes:####
+# Temperature ####
+# gas exchange ####
+# 
